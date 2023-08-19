f0d324dc8ee3fb5d98b1a1ef4962794809396642
Bali Babu Chauhan <balibabuchauhan1@gmail.com>
2023-08-20 00:09:53 +0545

    updating timeline

---
+++ branch_automater.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+SETLOCAL
+
+REM Get the current date in the format yyyymmdd
+FOR /F "tokens=1-3 delims=/ " %%A IN ("%DATE%") DO (
+    SET "YYYY=%%C"
+    SET "MM=00%%A"
+    SET "DD=00%%B"
+)
+SET "TODAY=%YYYY%%MM:~-2%%DD:~-2%"
+
+ECHO ====== %TODAY% ========
+ECHO Auto pulling, adding, committing, and pushing on Github.
+
+ECHO -------- Pulling --------
+git pull
+git status
+
+ECHO -------- Adding --------
+git add .
+git status
+
+ECHO -------- Committing --------
+git commit -m "updating timeline"
+
+ECHO -------- Creating and Pushing Branch --------
+git checkout -b %TODAY%
+git push origin %TODAY%
+
+ECHO Don't Just Have a Good Day, Have a Great Day
+PAUSE
+exit
